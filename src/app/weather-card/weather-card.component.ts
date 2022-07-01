d606e8c5f8f24f17c8ed0a2387183cf1ff3eae96
Chandinibasha786 <chandinibasha786@gmail.com>
2022-07-01 13:15:53 +0530

    modified the if statements to switch

--- src/app/weather-card/weather-card.component.ts
+++ src/app/weather-card/weather-card.component.ts
@@ -46,30 +46,49 @@ ngOnInit(): void {
   }
 
   getBackground(){
-    if(this.weather == 'Clouds'){
-      return 'https://images.unsplash.com/photo-1506563805286-a52f937ec9ed?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
-    } 
-    else if(this.weather == 'Rain'){
-      return 'https://images.unsplash.com/photo-1493314894560-5c412a56c17c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
+    switch (this.weather) {
+      case 'Clouds':
+        return 'https://images.unsplash.com/photo-1506563805286-a52f937ec9ed?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
+      case 'Rain':
+        return 'https://images.unsplash.com/photo-1493314894560-5c412a56c17c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
+      case 'Haze':
+        return 'https://images.unsplash.com/photo-1554511491-0f21bacd5ba0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80'
+      case 'Thunderstorm':
+        return 'https://images.unsplash.com/photo-1492011221367-f47e3ccd77a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'
+      case 'Drizzle':
+        return 'https://images.unsplash.com/photo-1541919329513-35f7af297129?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
+      case 'Snow':
+        return 'https://images.unsplash.com/photo-1485594050903-8e8ee7b071a8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=812&q=80'
+      case 'Clear':
+        return 'https://images.unsplash.com/photo-1454335459109-cd8a2c230bc2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=930&q=80'
+      default:
+        return '';
     }
-    else if(this.weather == 'Haze'){
-      return 'https://images.unsplash.com/photo-1554511491-0f21bacd5ba0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80'
-    }
-    else if(this.weather == 'Thunderstorm'){
-      return 'https://images.unsplash.com/photo-1492011221367-f47e3ccd77a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'
-    }
-    else if(this.weather == 'Drizzle'){
-      return 'https://images.unsplash.com/photo-1541919329513-35f7af297129?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
-    }
-    else if(this.weather == 'Snow'){
-      return 'https://images.unsplash.com/photo-1485594050903-8e8ee7b071a8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=812&q=80'
-    }
-    else if(this.weather == 'Clear'){
-      return 'https://images.unsplash.com/photo-1454335459109-cd8a2c230bc2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=930&q=80'
-    }
-    else{
-      return ''
-    }
-  }
+  //   if(this.weather == 'Clouds'){
+  //     return 'https://images.unsplash.com/photo-1506563805286-a52f937ec9ed?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
+  //   } 
+  //   else if(this.weather == 'Rain'){
+  //     return 'https://images.unsplash.com/photo-1493314894560-5c412a56c17c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
+  //   }
+  //   else if(this.weather == 'Haze'){
+  //     return 'https://images.unsplash.com/photo-1554511491-0f21bacd5ba0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80'
+  //   }
+  //   else if(this.weather == 'Thunderstorm'){
+  //     return 'https://images.unsplash.com/photo-1492011221367-f47e3ccd77a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'
+  //   }
+  //   else if(this.weather == 'Drizzle'){
+  //     return 'https://images.unsplash.com/photo-1541919329513-35f7af297129?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
+  //   }
+  //   else if(this.weather == 'Snow'){
+  //     return 'https://images.unsplash.com/photo-1485594050903-8e8ee7b071a8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=812&q=80'
+  //   }
+  //   else if(this.weather == 'Clear'){
+  //     return 'https://images.unsplash.com/photo-1454335459109-cd8a2c230bc2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=930&q=80'
+  //   }
+  //   else{
+  //     return ''
+  //   }
+  // }
 
+}
 }
